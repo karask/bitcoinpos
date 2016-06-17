9ea6d08e37a62e30f5f4e348c64fbe5b718f2230
karask <kkarasavvas@gmail.com>
2016-06-17 17:45:41 +0300

    preferences are properly initialized, updated and saved

--- app/src/main/java/com/cryptocurrencies/bitcoinpos/SettingsActivity.java
+++ app/src/main/java/com/cryptocurrencies/bitcoinpos/SettingsActivity.java
@@ -3,12 +3,22 @@ package com.cryptocurrencies.bitcoinpos;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.support.v7.preference.DialogPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -28,6 +38,7 @@ public class SettingsActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.content_frame, new SettingsFragment())
                 .commit();
+
     }
 
 
@@ -39,7 +50,7 @@ public class SettingsActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if(id == android.R.id.home) {
+        if (id == android.R.id.home) {
             NavUtils.navigateUpFromSameTask(this);
             return true;
         }
@@ -48,16 +59,64 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
 
-    public static class SettingsFragment extends PreferenceFragmentCompat {
+
+
+
+    public static class SettingsFragment extends PreferenceFragmentCompat  implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+        SharedPreferences sharedPreferences;
 
         @Override
         public void onCreatePreferences(Bundle bundle, String s) {
             // Load the preferences from an XML resource
             addPreferencesFromResource(R.xml.preferences);
 
-            android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) getActivity().findViewById(R.id.toolbar);
-            ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+            Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
+            ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+            // Set preferences' values to summaries by reusing onSharedPreferenceChanged
+            sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            onSharedPreferenceChanged(sharedPreferences, getString(R.string.merchant_name_key));
+            onSharedPreferenceChanged(sharedPreferences, getString(R.string.payment_address_key));
+            onSharedPreferenceChanged(sharedPreferences, getString(R.string.local_currency_key));
         }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            //unregister the preferenceChange listener
+            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            Preference preference = findPreference(key);
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int prefIndex = listPreference.findIndexOfValue(sharedPreferences.getString(key, ""));
+                if (prefIndex >= 0) {
+                    preference.setSummary(listPreference.getEntries()[prefIndex]);
+                }
+            } else {
+                preference.setSummary(sharedPreferences.getString(key, ""));
+            }
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            //unregister the preference change listener
+            getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        }
+
+//        @Override
+//        public void onDestroy() {
+//            super.onDestroy();
+//            //unregister event bus.
+//            EventBus.unregister(this);
+//        }
+
+
     }
 
 }
