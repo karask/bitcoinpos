4b07e52001590d5606192f0b78485d73b8a42d1a
karask <kkarasavvas@gmail.com>
2016-07-01 16:32:28 +0300

    All keypads are working plus basic currency toggle button

--- app/src/main/java/com/cryptocurrencies/bitcoinpos/PaymentFragment.java
+++ app/src/main/java/com/cryptocurrencies/bitcoinpos/PaymentFragment.java
@@ -1,21 +1,31 @@
 package com.cryptocurrencies.bitcoinpos;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 /**
  * Created by kostas on 14/6/2016.
  */
 public class PaymentFragment extends Fragment implements View.OnClickListener {
 
+    private final double maxBTCAmount = 1000;
+
     Button keypad1, keypad2, keypad3, keypad4, keypad5, keypad6, keypad7, keypad8, keypad9, keypad0, keypadDot, keypadBackspace;
+    ToggleButton currencyToggle;
     TextView amount;
+
+    SharedPreferences sharedPreferences;
+
     public PaymentFragment() {
         // Required empty public constructor
     }
@@ -29,10 +39,19 @@ public class PaymentFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        // get preferences
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String localCurrency = sharedPreferences.getString(getString(R.string.local_currency_key), "NaN");
+
         // Inflate the layout for this fragment
         View fragmentView = inflater.inflate(R.layout.fragment_payment, container, false);
 
         amount = (TextView) fragmentView.findViewById(R.id.amountTextView);
+
+        currencyToggle = (ToggleButton) fragmentView.findViewById(R.id.currencyToggleButton);
+        currencyToggle.setTextOff("BTC");
+        currencyToggle.setTextOn(localCurrency);
+        currencyToggle.setOnClickListener(this);
 
         keypad1 = (Button) fragmentView.findViewById(R.id.btn_1);
         keypad1.setOnClickListener(this);
@@ -64,16 +83,75 @@ public class PaymentFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_backspace:
-                break;
-            default:
-                if(amount.getText().equals("0")) {
-                    amount.setText("1");
-                } else {
-                    amount.append("2");
-                }
+        if(v instanceof ToggleButton) {
+            ToggleButton  currencyToggle = (ToggleButton) v;
+            String currentAmount = amount.getText().toString();
+            if(currencyToggle.isChecked()) {
+                // was local currency - convert to BTC
+            } else {
+                // was BTC - convert to local currency
+            }
+        } else {
+            // it is a button!
+            Button keypad = (Button) v;
+            switch (keypad.getId()) {
+                case R.id.btn_backspace:
+                    String currentAmount = amount.getText().toString();
+                    // delete last digit
+                    currentAmount = currentAmount.substring(0, currentAmount.length() - 1);
+                    if (currentAmount.length() == 0)
+                        amount.setText("0");
+                    else
+                        amount.setText(currentAmount);
+                    break;
+                case R.id.btn_dot:
+                    // if another dot does not exists
+                    if (amount.getText().toString().indexOf(".") == -1) {
+                        amount.append(".");
+                    }
 
+                    break;
+                case R.id.btn_0:
+                case R.id.btn_1:
+                case R.id.btn_2:
+                case R.id.btn_3:
+                case R.id.btn_4:
+                case R.id.btn_5:
+                case R.id.btn_6:
+                case R.id.btn_7:
+                case R.id.btn_8:
+                case R.id.btn_9:
+                    if (amount.getText().toString().equals("0")) {
+                        amount.setText(keypad.getText().toString());
+                    } else {
+                        String newAmount = amount.getText().toString() + keypad.getText().toString();
+                        if(checkValidAmount(newAmount)) {
+                            amount.setText(newAmount);
+                        } else {
+                            Toast.makeText(getContext(), R.string.amount_less_than_1000_message, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    break;
+
+
+            }
         }
+
+
+    }
+
+    private boolean checkValidAmount(String amount) {
+
+        double doubleAmount = Double.parseDouble(amount);
+        if(currencyToggle.isChecked()) {
+            // was local currency
+        } else {
+            // was BTC
+            if(doubleAmount > maxBTCAmount)
+                return false;
+        }
+
+
+        return true;
     }
 }
