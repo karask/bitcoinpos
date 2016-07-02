8caf371c42af7d9bffd708e189dc6ff1371ccb7b
karask <kkarasavvas@gmail.com>
2016-07-02 12:17:25 +0300

    Ensures proper (snackbar) message is displayed if invalid bitcoin address

--- app/src/main/java/com/cryptocurrencies/bitcoinpos/PaymentFragment.java
+++ app/src/main/java/com/cryptocurrencies/bitcoinpos/PaymentFragment.java
@@ -1,8 +1,11 @@
 package com.cryptocurrencies.bitcoinpos;
 
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.preference.PreferenceManager;
 import android.view.LayoutInflater;
@@ -21,10 +24,13 @@ public class PaymentFragment extends Fragment implements View.OnClickListener {
     private final double maxBTCAmount = 1000;
 
     Button keypad1, keypad2, keypad3, keypad4, keypad5, keypad6, keypad7, keypad8, keypad9, keypad0, keypadDot, keypadBackspace;
+    Button requestPayment;
     ToggleButton currencyToggle;
     TextView amount;
+    CoordinatorLayout coordinatorLayout;
 
     SharedPreferences sharedPreferences;
+    String bitcoinPaymentAddress ;
 
     public PaymentFragment() {
         // Required empty public constructor
@@ -32,7 +38,9 @@ public class PaymentFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
@@ -42,9 +50,12 @@ public class PaymentFragment extends Fragment implements View.OnClickListener {
         // get preferences
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
         String localCurrency = sharedPreferences.getString(getString(R.string.local_currency_key), "NaN");
+        bitcoinPaymentAddress = sharedPreferences.getString(getString(R.string.payment_address_key), "");
 
         // Inflate the layout for this fragment
         View fragmentView = inflater.inflate(R.layout.fragment_payment, container, false);
+
+        coordinatorLayout = (CoordinatorLayout) fragmentView.findViewById(R.id.coordinatorLayout);
 
         amount = (TextView) fragmentView.findViewById(R.id.amountTextView);
 
@@ -77,6 +88,9 @@ public class PaymentFragment extends Fragment implements View.OnClickListener {
         keypadDot.setOnClickListener(this);
         keypadBackspace = (Button) fragmentView.findViewById(R.id.btn_backspace);
         keypadBackspace.setOnClickListener(this);
+
+        requestPayment = (Button) fragmentView.findViewById(R.id.request_payment);
+        requestPayment.setOnClickListener(this);
 
         return fragmentView;
     }
@@ -125,7 +139,7 @@ public class PaymentFragment extends Fragment implements View.OnClickListener {
                         amount.setText(keypad.getText().toString());
                     } else {
                         String newAmount = amount.getText().toString() + keypad.getText().toString();
-                        if(checkValidAmount(newAmount)) {
+                        if (checkValidAmount(newAmount)) {
                             amount.setText(newAmount);
                         } else {
                             Toast.makeText(getContext(), R.string.amount_less_than_1000_message, Toast.LENGTH_SHORT).show();
@@ -133,7 +147,20 @@ public class PaymentFragment extends Fragment implements View.OnClickListener {
                     }
                     break;
 
+                case R.id.request_payment:
+                    if (bitcoinPaymentAddress.isEmpty() || BitcoinUtils.validateAddress(bitcoinPaymentAddress)) {
+                        Snackbar mesg = Snackbar.make(coordinatorLayout, R.string.specify_valid_bitcoin_address_message, Snackbar.LENGTH_INDEFINITE)
+                                .setAction("Settings", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent goToSettings = new Intent(getContext(), SettingsActivity.class);
+                                startActivity(goToSettings);
+                            }
 
+                        });
+                        mesg.show();
+                    }
+                    break;
             }
         }
 
